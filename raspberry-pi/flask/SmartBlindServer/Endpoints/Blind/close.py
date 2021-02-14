00df2cc99dd04d91062d9b2c0ce9c211adec3b51
Jason Hough <jasonhough0@gmail.com>
2021-02-14 00:13:11 -0500

    updates endpoints to use auth

--- raspberry-pi/flask/SmartBlindServer/Endpoints/Blind/close.py
+++ raspberry-pi/flask/SmartBlindServer/Endpoints/Blind/close.py
@@ -1,5 +1,8 @@
 from flask_restful import Resource
+from SmartBlindServer.Core.authentication import auth
+
 
 class BlindCloseResource(Resource):
+    @auth.login_required
     def put(self):
         return {"result": "closed"}
--- raspberry-pi/flask/SmartBlindServer/Endpoints/Blind/open.py
+++ raspberry-pi/flask/SmartBlindServer/Endpoints/Blind/open.py
@@ -1,5 +1,8 @@
 from flask_restful import Resource
+from SmartBlindServer.Core.authentication import auth
+
 
 class BlindOpenResource(Resource):
+    @auth.login_required
     def put(self):
         return {"result": "opened"}
