dfedc86f555aaf894ff7f36762d4301f7d08c621
Jason Hough <jasonhough0@gmail.com>
2021-04-26 15:28:01 -0400

    uncomments flask auth statements

--- raspberry-pi/flask/SmartBlindServer/Endpoints/Blind/close.py
+++ raspberry-pi/flask/SmartBlindServer/Endpoints/Blind/close.py
@@ -1,12 +1,12 @@
 from flask_restful import Resource
-#from SmartBlindServer.Core.authentication import auth
+from SmartBlindServer.Core.authentication import auth
 
 
 class BlindCloseResource(Resource):
     def __init__(self, **kwargs):
         self.motor = kwargs["motor"]
 
-#    @auth.login_required
+    @auth.login_required
     def put(self):
         self.motor.close()
         return {"result": "opened" if self.motor.status() else "closed"}
